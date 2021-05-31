ebea01ac893ab0cc3ba6267f34ffa92f88eff0f9
kennetha123 <jodysag5@gmail.com>
2021-06-01 02:06:06 +0700

    fix make file build

--- project/premake5.lua
+++ project/premake5.lua
@@ -9,16 +9,16 @@ project "ImGui"
   
   files
   {
-    "imconfig.h",
-    "imgui.h",
-    "imgui.cpp",
-    "imgui_draw.cpp",
-    "imgui_internal.h",
-    "imgui_widgets.cpp",
-    "imstb_rectpack.h",
-    "imstb_textedit.h",
-    "imstb_truetype.h",
-    "imgui_demo.cpp"
+    "../imconfig.h",
+    "../imgui.h",
+    "../imgui.cpp",
+    "../imgui_draw.cpp",
+    "../imgui_internal.h",
+    "../imgui_widgets.cpp",
+    "../imstb_rectpack.h",
+    "../imstb_textedit.h",
+    "../imstb_truetype.h",
+    "../imgui_demo.cpp"
   }
   
   filter "system:windows"
